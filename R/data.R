4deadab1ac7d78aa4769668e551e6a7cbaf6e25e
Jack Talboys <jtalboys@mango-solutions.com>
2019-09-14 10:10:16 +0100

    Added URL to DESC, also new exported dataset teams

--- R/data.R
+++ R/data.R
@@ -11,3 +11,20 @@
 #' }
 #' @source \url{https://fantasy.premierleague.com/api/bootstrap-static/}
 "id_lookup"
+
+#' Data for each PL team
+#' 
+#' Mainly used to convert ID's to actual team names, but the strength assigned
+#' by the FPL has been left in just in case it is of any interest.
+#' 
+#' @format A data frame with 20 rows and 10 variables:
+#' \describe{
+#'    \item{id}{id for each team}
+#'    \item{name}{full name}
+#'    \item{short_name}{abbreviated name}
+#'    \item{strength}{Overall strenght value assigned by FPL algorithm}
+#'    \item{stength_*}{Breakdown of a teams strengths, split in to overall, attacking
+#'    and defending for home and away}
+#'}
+#'@source \url{https://fantasy.premierleague.com/api/bootstrap-static/}
+"teams"
